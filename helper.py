0788267b3cfd963c022f3710bac471a3aa4e3933
Sai Eshwari <sai.eshwari@cmr.edu.in>
2025-12-16 23:01:31 +0530

    Initial commit - WhatsApp Chat Analyzer

---
+++ helper.py
@@ -0,0 +1,84 @@
+from urlextract import URLExtract
+from wordcloud import WordCloud
+from collections import Counter
+import pandas as pd
+
+extract = URLExtract()
+
+# ---------------------- Fetch Stats ----------------------
+def fetch_stats(selected_user, df):
+
+    if selected_user != 'Overall':
+        df = df[df['user'] == selected_user]
+
+    num_messages = df.shape[0]
+    words = []
+    for message in df['message']:
+        words.extend(message.split())
+
+    # fetch number of media messages
+    num_media_messages = df[df['message'].str.contains('<Media omitted>', case=False, na=False)].shape[0]
+
+    # fetch number of links shared
+    links = []
+    for message in df['message']:
+        links.extend(extract.find_urls(message))
+
+    return num_messages, len(words), num_media_messages, len(links)
+
+
+# ---------------------- Most Busy Users ----------------------
+def most_busy_users(df):
+    # top 5 most active users
+    x = df['user'].value_counts().head()
+    # percentage contribution of each user
+    df_percent = round((df['user'].value_counts() / df.shape[0]) * 100, 2).reset_index()
+    # rename columns properly
+    df_percent.columns = ['name', 'percent']
+
+    return x, df_percent
+
+
+# ---------------------- WordCloud ----------------------
+def create_wordcloud(selected_user, df):
+    # load stopwords
+    with open(r"D:\Finalyearproject\stop_hinglish.txt", "r", encoding="utf-8") as f:
+        stop_words = f.read()
+
+    if selected_user != 'Overall':
+        df = df[df['user'] == selected_user]
+
+    # remove group notifications and media messages
+    temp = df[df['user'] != 'group_notification']
+    temp = temp[temp['message'] != '<Media omitted>\n']
+
+    # join all messages
+    text = " ".join(temp['message'])
+
+    # generate wordcloud
+    wc = WordCloud(width=500, height=500, min_font_size=10, background_color='white')
+    df_wc = wc.generate(text)
+
+    return df_wc
+
+
+# ---------------------- Most Common Words ----------------------
+def most_common_words(selected_user, df):
+    with open(r"D:\Finalyearproject\stop_hinglish.txt", "r", encoding="utf-8") as f:
+        stop_words = f.read().split()
+
+    if selected_user != 'Overall':
+        df = df[df['user'] == selected_user]
+
+    temp = df[df['user'] != 'group_notification']
+    temp = temp[temp['message'] != '<Media omitted>\n']
+
+    words = []
+    for message in temp['message']:
+        for word in message.lower().split():
+            if word not in stop_words and word != '<media' and word != 'omitted>':
+                words.append(word)
+
+    most_common_df = pd.DataFrame(Counter(words).most_common(20))
+
+    return most_common_df
